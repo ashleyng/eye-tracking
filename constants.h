60a59de7bda21249d67aacb07ddce150b32495ac
ashleyng <ashley.ng@utexas.edu>
2015-07-05 15:52:58 -0500

    clean up and comment code

---
+++ constants.h
@@ -0,0 +1,10 @@
+
+// eye size rations
+const int kEyePercentWidth = 30;
+const int kEyePercentHeight = 25;
+const int kEyePercentTop = 25;
+const int kEyePercentSide = 15;
+
+const int kScaledEyeWidth = 50;
+const double kGradientThreshold = 50.0;
+const int kFastEyeWidth = 50;
--- main.cpp
+++ main.cpp
@@ -2,23 +2,21 @@
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
+#include "constants.h"
 
 using namespace std;
 using namespace cv;
 
-const int kSmoothFaceFactor = 0.005;
-const int kEyePercentWidth = 30;
-const int kEyePercentHeight = 25;
-const int kEyePercentTop = 25;
-const int kEyePercentSide = 15;
-const int kScaledEyeWidth = 50;
-const double kGradientThreshold = 50.0;
-const int kFastEyeWidth = 50;
 
-
-
-void scale(const cv::Mat &src,cv::Mat &dst) {
+void scale(const Mat &src,Mat &dst) {
     cv::resize(src, dst, cv::Size(kFastEyeWidth,(((float)kFastEyeWidth)/src.cols) * src.rows));
+}
+
+Point unscale_point(Point p, Rect origSize) {
+    float ratio = (((float)kFastEyeWidth)/origSize.width);
+    int x = round(p.x / ratio);
+    int y = round(p.y / ratio);
+    return cv::Point(x,y);
 }
 
 Mat matrix_magnitude(Mat mat_x, Mat mat_y) {
@@ -35,73 +33,68 @@ Mat matrix_magnitude(Mat mat_x, Mat mat_y) {
     }
     return mag;
 }
-double computeDynamicThreshold(const cv::Mat &mat, double stdDevFactor) {
-    cv::Scalar stdMagnGrad, meanMagnGrad;
-    cv::meanStdDev(mat, meanMagnGrad, stdMagnGrad);
-    double stdDev = stdMagnGrad[0] / sqrt(mat.rows*mat.cols);
-    return stdDevFactor * stdDev + meanMagnGrad[0];
-}
 
+
+/*
+ * Find possible center in gradient location
+ * doesn't use the postprocessing weight of color (section 2.1)
+ */
 void possible_centers(int x, int y, const Mat &blurred, double gx, double gy, Mat &output) {
 
     for (int cy = 0; cy < output.rows; cy++) {
         double *output_row = output.ptr<double>(cy);
-        const unsigned  char *blur_row = blurred.ptr<unsigned char>(cy);
         for (int cx = 0; cx < output.cols; cx++) {
             if (x == cx && y == cy) {
                 continue;
             }
+            // equation (2)
             double dx = x - cx;
             double dy = y - cy;
             double magnitude = sqrt((dx * dx) + (dy * dy));
             dx = dx / magnitude;
             dy = dy / magnitude;
-            double dotProduct = dx*gx + dy*gy;
+
+            double dotProduct = (dx*gx + dy*gy);
+            // ignores vectors pointing in opposite direction/negative dot products
             dotProduct = max(0.0, dotProduct);
 
+            // summation
             output_row[cx] += dotProduct * dotProduct;
         }
     }
 }
 
-Point unscalePoint(cv::Point p, cv::Rect origSize) {
-    float ratio = (((float)kFastEyeWidth)/origSize.width);
-    int x = round(p.x / ratio);
-    int y = round(p.y / ratio);
-    return cv::Point(x,y);
-}
-
+/*
+ * Finds the pupils within the given eye region
+ * returns points of where pupil is calculated to be
+ *
+ * face_image: image of face region from frame
+ * eye_region: dimensions of eye region
+ * window_name: display window name
+ */
 Point find_centers(Mat face_image, Rect eye_region, string window_name) {
     Mat eye_unscaled = face_image(eye_region);
+
+    // scale and grey image
     Mat eye_scaled_gray;
     scale(eye_unscaled, eye_scaled_gray);
     cvtColor(eye_scaled_gray, eye_scaled_gray, COLOR_BGRA2GRAY);
+
+    // get the gradient of eye regions
     Mat gradient_x, gradient_y;
     Sobel(eye_scaled_gray, gradient_x, CV_64F, 1, 0, 5);
     Sobel(eye_scaled_gray, gradient_y, CV_64F, 0, 1, 5);
     Mat magnitude = matrix_magnitude(gradient_x, gradient_y);
-    double gradientThresh = computeDynamicThreshold(magnitude, kGradientThreshold);
 
-    for (int y = 0; y < eye_scaled_gray.rows; y++) {
-        double *x_row = gradient_x.ptr<double>(y), *y_row = gradient_y.ptr<double>(y);
-        const double *mag_row = magnitude.ptr<double>(y);
-        for (int x = 0; x < eye_scaled_gray.cols; x++) {
-            double gx = x_row[x], gy = y_row[x];
-            double mag = mag_row[x];
-            if (mag > gradientThresh) {
-                x_row[x] = gx/mag;
-                y_row[x] = gy/mag;
-            }
-            else {
-                x_row[x] = 0.0;
-                y_row[x] = 0.0;
-            }
-        }
-    }
+    // normalized displacement vectors
+    normalize(gradient_x, gradient_x);
+    normalize(gradient_y, gradient_y);
 
+    // blur and invert the image
     Mat blurred;
     GaussianBlur(eye_scaled_gray, blurred, Size(5, 5), 0, 0);
     bitwise_not(blurred, blurred);
+
 
     Mat outSum = Mat::zeros(eye_scaled_gray.rows, eye_scaled_gray.cols, CV_64F);
 
@@ -120,15 +113,19 @@ Point find_centers(Mat face_image, Rect eye_region, string window_name) {
     Mat out;
     outSum.convertTo(out, CV_32F, 1.0/numGradients);
 
-    Point maxP;
-    double maxVal;
-    minMaxLoc(out, NULL, &maxVal, NULL, &maxP);
-    Point pupil = unscalePoint(maxP, eye_region);
+    Point max_point;
+    double max_value;
+    minMaxLoc(out, NULL, &max_value, NULL, &max_point);
+    Point pupil = unscale_point(max_point, eye_region);
     return pupil;
 }
 
-
+/*
+ * color_image: image of the whole frame
+ * face: dimensions of face in color_image
+ */
 void find_eyes(Mat color_image, Rect face) {
+    // image of face
     Mat face_image = color_image(face);
 
     int eye_width = face.width * (kEyePercentWidth/100.0);
@@ -137,21 +134,25 @@ void find_eyes(Mat color_image, Rect face) {
     int eye_side = face.width * (kEyePercentSide/100.0);
     int right_eye_x = face.width - eye_width -  eye_side;
 
-    // left eye
+    // eye regions
     Rect left_eye_region(eye_side, eye_top, eye_width, eye_height);
     Rect right_eye_region(right_eye_x, eye_top, eye_width, eye_height);
 
+    // draw eye regions
     rectangle(face_image, left_eye_region, Scalar(0, 0, 255));
     rectangle(face_image, right_eye_region, Scalar(0, 0, 255));
 
+    // get points of pupils
     Point left_pupil = find_centers(face_image, left_eye_region, "left eye");
     Point right_pupil = find_centers(face_image, right_eye_region, "right eye");
 
+    // convert points to fit on frame image
     right_pupil.x += right_eye_region.x;
     right_pupil.y += right_eye_region.y;
     left_pupil.x += left_eye_region.x;
     left_pupil.y += left_eye_region.y;
 
+    // draw pupils
     circle(face_image, right_pupil, 3, Scalar(0, 255, 0));
     circle(face_image, left_pupil, 3, Scalar(0, 255, 0));
 
@@ -179,9 +180,9 @@ int main() {
         cvtColor(frame, gray_image, COLOR_BGRA2GRAY);
         face_cascade.detectMultiScale(gray_image, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE|CV_HAAR_FIND_BIGGEST_OBJECT);
 
-        for (int x = 0; x < faces.size(); x++) {
-            rectangle(frame, faces[0], 1234);
-        }
+//        for (int x = 0; x < faces.size(); x++) {
+//            rectangle(frame, faces[0], 1234);
+//        }
 
         if (faces.size() > 0) {
             find_eyes(frame, faces[0]);
